664d07eac1ebbbe77d7dbafbdc02fe9751bd1d5b
Helen Darlington <b9015917@newcastle.ac.uk>
2021-11-01 12:50:18 +0000

    Allowing user to change info

--- CSC3131/DataHope/database_queries.py
+++ CSC3131/DataHope/database_queries.py
@@ -2,6 +2,9 @@ from flask import(
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
+from werkzeug.security import check_password_hash, generate_password_hash
+from .db import get_db
+
 bp = Blueprint('database_queries', __name__, url_prefix='/database_queries')
 
 @bp.route('/add_data')
@@ -12,6 +15,34 @@ def add_data():
 def get_data():
     return render_template('/database_queries/get_data.html')
 
-@bp.route('/change_info')
+@bp.route('/change_info', methods=('GET', 'POST'))
 def change_info():
+    if request.method =='POST':
+        username = request.form['username']
+        password = request.form['password']
+        charity = request.form['charity']
+        email = request.form['email']
+
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required'
+        elif not password:
+            error = 'Password is required'
+
+        if error is None:
+            try:
+                db.execute(
+                    "UPDATE user SET password = ?, charity = ?, email = ? WHERE username = ?;",
+                    (generate_password_hash(password), charity, email, username),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = "error in changing info"
+            else:
+                return redirect(url_for("home.home"))
+
+
+        flash(error)
     return render_template('/database_queries/change_info.html')
